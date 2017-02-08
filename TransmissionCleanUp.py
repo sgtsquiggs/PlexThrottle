8292c1187de2b25a4bbdbcc397e7836ebe1e3788
Matthew Crenshaw <sgt@squig.gs>
2017-02-07 23:15:29 -0500

    nyaa is now anime

--- TransmissionCleanUp.py
+++ TransmissionCleanUp.py
@@ -9,10 +9,10 @@ TRANS_PORT = config['TRANS_PORT']
 TRANS_USER = config['TRANS_USER']
 TRANS_PASS = config['TRANS_PASS']
 TRANS_PUBLIC_RATIO_LIMIT = config['TRANS_PUBLIC_RATIO_LIMIT']
-TRANS_NYAA_RATIO_LIMIT = config['TRANS_NYAA_RATIO_LIMIT']
+TRANS_ANIME_RATIO_LIMIT = config['TRANS_ANIME_RATIO_LIMIT']
 
 
-def update_nyaa_torrents(host, port, user, password, ratio=TRANS_NYAA_RATIO_LIMIT):
+def update_nyaa_torrents(host, port, user, password, ratio=TRANS_ANIME_RATIO_LIMIT):
     tc = transmissionrpc.Client(host, port=port, user=user, password=password)
 
     # All torrents
@@ -21,12 +21,9 @@ def update_nyaa_torrents(host, port, user, password, ratio=TRANS_NYAA_RATIO_LIMI
     # Only public torrents
     torrents = filter(lambda t: not t.isPrivate, torrents)
 
-    # Only torrents using nyaa.se tracker
-    torrents = list(filter(
-        lambda t: reduce(
-            lambda result, x: result or 'nyaa' in x['announce'] or 'wakku' in x['announce'], t.trackers, False
-        ) is True, torrents
-    ))
+    # Only torrents with matching trackers
+    trackers = ['nyaa', 'wakku']
+    torrents = list(filter(lambda t: reduce(lambda result, x: result or any(s in x['announce'] for s in trackers), t.trackers, False) is True, torrents))
 
     # Torrent ids
     ids = list(map(lambda t: t.id, torrents))
@@ -99,7 +96,7 @@ def delete_completed_public_stopped_torrents(host, port, user, password):
 
 
 num_changed = len(update_global_ratio_public_torrents(TRANS_HOST, TRANS_PORT, TRANS_USER, TRANS_PASS, TRANS_PUBLIC_RATIO_LIMIT))
-num_changed += len(update_nyaa_torrents(TRANS_HOST, TRANS_PORT, TRANS_USER, TRANS_PASS, TRANS_NYAA_RATIO_LIMIT))
+num_changed += len(update_nyaa_torrents(TRANS_HOST, TRANS_PORT, TRANS_USER, TRANS_PASS, TRANS_ANIME_RATIO_LIMIT))
 num_stopped = len(stop_completed_public_seeding_torrents(TRANS_HOST, TRANS_PORT, TRANS_USER, TRANS_PASS))
 num_deleted = len(delete_completed_public_stopped_torrents(TRANS_HOST, TRANS_PORT, TRANS_USER, TRANS_PASS))
 
